089bc9c6e4561172b7bc9950dbd812d68583d975
Ambrose Chua <ambrose@chua.family>
2019-09-04 15:09:05 +0800

    Add SSH connection option

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
-module "github.com/productionwentdown/forward"
+module github.com/productionwentdown/forward
 
 require (
-	"github.com/judwhite/go-svc" v1.0.0
-	"golang.org/x/sys" v0.0.0-20180322165403-91ee8cde4354
+	github.com/judwhite/go-svc v1.0.0
+	golang.org/x/sys v0.0.0-20180322165403-91ee8cde4354 // indirect
 )
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"io"
 	"log"
@@ -9,13 +10,16 @@ import (
 
 var listen string
 var connect string
+var connectSSH string
 
 var ln net.Listener
 var conn *net.TCPAddr
+var connSSH *net.TCPAddr
 
 func setup() {
-	flag.StringVar(&listen, "listen", ":8000", "listen on ip and port")
-	flag.StringVar(&connect, "connect", "", "forward to ip and port")
+	flag.StringVar(&listen, "listen", ":8000", "listen on address")
+	flag.StringVar(&connect, "connect", "", "forward to address")
+	flag.StringVar(&connectSSH, "ssh", "", "if set, will do basic introspection to forward SSH traffic to this address")
 	flag.Parse()
 
 	var err error
@@ -27,6 +31,12 @@ func setup() {
 		log.Fatal(err)
 	}
 
+	// check and parse SSH address
+	connSSH, _ = net.ResolveTCPAddr("tcp", connectSSH)
+	if connectSSH == "" {
+		connSSH = nil
+	}
+
 	// listen on address
 	ln, err = net.Listen("tcp", listen)
 	if err != nil {
@@ -36,6 +46,9 @@ func setup() {
 
 	log.Printf("listening on %v", ln.Addr())
 	log.Printf("will connect to %v", conn)
+	if connSSH != nil {
+		log.Printf("will connect SSH to %v", connSSH)
+	}
 }
 
 func serve() {
@@ -48,17 +61,46 @@ func serve() {
 		}
 
 		log.Printf("connection %v from %v", i, c.RemoteAddr())
-
-		cn, err := net.DialTCP("tcp", nil, conn)
-		if err != nil {
-			c.Close()
-			log.Print(err)
-			continue
-		}
-
-		go pipe(c, cn, i)
-		go pipe(cn, c, i)
+		go handle(c, i)
 	}
+}
+
+var magic = []byte{'S', 'S', 'H', '-'}
+
+var magicLen = len(magic)
+
+func handle(c net.Conn, count int) {
+	// read first four characters
+	readMagic := make([]byte, magicLen, magicLen)
+	n, err := c.Read(readMagic)
+	if n != magicLen {
+		log.Printf("warning! could not read header")
+		return
+	}
+	opError, ok := err.(*net.OpError)
+	if err != nil && (!ok || opError.Op != "readfrom") {
+		log.Printf("warning! %v", err)
+		return
+	}
+
+	connTo := conn
+	// if the header looks like SSH, forward to SSH connection
+	if bytes.Equal(readMagic, magic) {
+		connTo = connSSH
+	}
+
+	cn, err := net.DialTCP("tcp", nil, connTo)
+	if err != nil {
+		c.Close()
+		log.Print(err)
+		return
+	}
+
+	// write the first four characters
+	cn.Write(readMagic)
+
+	go pipe(c, cn, count)
+	go pipe(cn, c, count)
 }
 
 func pipe(w io.WriteCloser, r io.ReadCloser, count int) {
